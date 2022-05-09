e1bd94495373db6c3f7cbe8b0ff0d666b7c24b2f
gaurav kumar <gauravkumargk9892@gmail.com>
2022-05-09 23:15:44 +0530

    update

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="form">
+        <form action="#">
+            <input type="text">
+            <button type="submit" class="btn search">Search</button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
---
+++ src/view/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        Hii
+        <SearchBar />
+    </div>
+</template>
+
+<script>
+import SearchBar from '../components/SearchBar.vue';
+
+export default {
+    components() {
+        SearchBar;
+    }
+}
+</script>
